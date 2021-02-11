9e73c1edfbdbd93967c120f472947e8367b1cd37
Ely Bourland <JEBourland@yahoo.com>
2021-02-10 16:12:24 -0800

    adding angular/flex-layout

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     AppRoutingModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
